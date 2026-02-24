ecf37eb604d7caa1ad76a63a1d7b3adcb6117691
Raven <familiar.omens@gmail.com>
2026-02-24 11:46:53 +0100

    Added RPC to pickupComponent

--- Project/Components/pickup_component.gd
+++ Project/Components/pickup_component.gd
@@ -12,17 +12,26 @@ func _component_attached():
 	connector = Components.get_first(ComponentConnector).on_ancestors_of(entity)
 
 func _physics_process(_delta: float) -> void:
+	is_multiplayer_authority()
 	if input.grap and not held_object:
 		for connection in connector.connections.values():
 			if IAmCollectible in connection:
 				var interface = connection.get(IAmCollectible) as IAmCollectible
-				held_object = interface._attached_to.entity
-				held_object.process_mode = Node.PROCESS_MODE_DISABLED
-				held_object.global_position = hold_point.global_position
-				held_object.reparent(hold_point)
+				grab.rpc(interface._attached_to.entity)
 	
 	if not input.grap and held_object:
 		if held_object.get_parent() == hold_point:
-			held_object.process_mode = Node.PROCESS_MODE_INHERIT
-			held_object.reparent(get_tree().root.get_child(0))
-		held_object = null
+			let_go.rpc()
+
+@rpc("call_local")
+func grab(node):
+	held_object = node
+	held_object.process_mode = Node.PROCESS_MODE_DISABLED
+	held_object.global_position = hold_point.global_position
+	held_object.reparent(hold_point)
+
+@rpc("call_local")
+func let_go():
+	held_object.process_mode = Node.PROCESS_MODE_INHERIT
+	held_object.reparent(get_tree().root.get_child(0))
+	held_object = null
